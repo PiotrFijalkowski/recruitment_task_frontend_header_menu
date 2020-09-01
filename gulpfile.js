4a6c92c5288f1fd73d4f00eae1a9fa31f577e800
Piotr Fijałkowski <seejay12@gmail.com>
2020-09-01 19:05:27 +0200

    #create src folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('app/sass/main.scss')
+  return gulp.src('app/src/sass/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./app/css'));
 });
  
 gulp.task('watch', function () {
-  gulp.watch('app/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('app/src/sass/**/*.scss', gulp.series('sass'));
 });
 
 gulp.task('dev', gulp.parallel('watch'));
